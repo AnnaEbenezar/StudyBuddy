bae99e508996efe8a23eddfd1009a50dce318e9b
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2022-11-22 16:07:45 +0700

    New Profile UI_1
    New GUI design

--- src/ProfileSystem/ProfileDriver.java
+++ src/ProfileSystem/ProfileDriver.java
@@ -1,10 +1,16 @@
 package ProfileSystem;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import MainSystem.MainDriver;
 import MainSystem.User;
+import Utility.CustomException;
 import Utility.ModuleDriver;
 
-import java.io.File;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class ProfileDriver implements ModuleDriver {
     private final MainDriver main;
@@ -12,8 +18,18 @@ public class ProfileDriver implements ModuleDriver {
     private volatile boolean runningFlag = false;
     private ProfileUI UI;
 
+    public Path path;
+    private static User user;
+    public String name;
+    public String major;
+    public String quote;
+
     private ProfileDriver(MainDriver main) {
         this.main = main;
+    }
+
+    public MainDriver getMain(){
+        return this.main;
     }
 
     public static ProfileDriver getInstance(MainDriver main) {
@@ -37,8 +53,74 @@ public class ProfileDriver implements ModuleDriver {
         UI = new ProfileUI(this);
         this.runningFlag = true;
 
+        user = main.getUser();
+        String username = "resources/users/" + user.getUsername() + "/profile.csv";
+        path = Paths.get(username);
 
     }
 
+    public void ProfileFileReader() {
+        File profileCSV = new File("resources/users/" + user.getUsername() + "/profile.csv");
+            try (Scanner reader = new Scanner(profileCSV);) {
+                
+
+                //  check if it is a username password csv file
+                if (!reader.nextLine().equals("name,major,quote")) {
+                    throw new FileNotFoundException();
+                }
+
+                while (reader.hasNextLine()) {
+                    String[] parts = reader.nextLine().split(",");
+                    name = parts[0];
+                    major = parts[1];
+                    quote = parts[2];
+                    
+                }
+                reader.close();
+
+            } catch (FileNotFoundException e) {
+                javax.swing.JOptionPane.showMessageDialog(null, "Profile data is missing. Creating empty file", "Warning", javax.swing.JOptionPane.WARNING_MESSAGE);
+                /* try {
+                    File dir = new File("resources/logindata");
+                    dir.mkdir();
+                    userPassCSV.createNewFile();
+                    FileWriter writer = new FileWriter(userPassCSV);
+                    writer.write("username,password");
+                    writer.close();
+                    return new HashMap<String, String>();
+
+                } catch (IOException fileEx) {
+                    JOptionPane.showMessageDialog(null, "Cannot create files.", "Error", JOptionPane.ERROR_MESSAGE);
+                    System.exit(1);
+                    return null;
+                } */
+            }
+
+    }
+
+    public void ProfileFileWriter() {
+        File profileCSV = new File("resources/users/" + user.getUsername() + "/profile.csv");
+            try {
+                FileWriter writer = new FileWriter(profileCSV);
+                writer.write("name,major,quote");
+                BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                bufferedWriter.newLine();
+                bufferedWriter.write(name);
+                bufferedWriter.write(",");
+                bufferedWriter.write(major);
+                bufferedWriter.write(",");
+                bufferedWriter.write(quote);
+                bufferedWriter.close(); 
+            } 
+            catch (IOException e) {
+                throw new CustomException.CreateAccountFileError();
+            }
+        }
+    }
+
+     
+
     
-}
+
+    
+
--- src/ProfileSystem/ProfileUI.java
+++ src/ProfileSystem/ProfileUI.java
@@ -1,222 +1,179 @@
 package ProfileSystem;
 
 import javax.swing.*;
+import java.awt.*;
+import MainSystem.User;
 
 public class ProfileUI extends JFrame {
 
+    private java.awt.Button button1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane_Accomplisments;
+    private javax.swing.JScrollPane jScrollPane_PerGoals;
+    private java.awt.Panel panel1;
+    private java.awt.Panel panel2;
+    private java.awt.Panel panel3;
+    private java.awt.TextArea textArea1;
+
     ProfileDriver driver;
     ProfileUI(ProfileDriver driver) {
+        this.driver = driver;
         initComponents();
         setVisible(true);
     }
 
     private void initComponents() {
 
-        panel1 = new java.awt.Panel();
-        jLabel_Profile = new javax.swing.JLabel();
-        button3 = new java.awt.Button();
-        panel2 = new java.awt.Panel();
-        label3 = new java.awt.Label();
-        textField3 = new java.awt.TextField();
-        label_FNAME = new java.awt.Label();
-        textField_FName = new java.awt.TextField();
-        label_LName = new java.awt.Label();
-        textField_LName = new java.awt.TextField();
-        label_DOB = new java.awt.Label();
-        textField_DOB = new java.awt.TextField();
-        label_PhoneNo = new java.awt.Label();
-        textField_PhoneNo = new java.awt.TextField();
-        label_Major = new java.awt.Label();
-        textField_Major = new java.awt.TextField();
-        button4 = new java.awt.Button();
-        button2 = new java.awt.Button();
-        panel3 = new java.awt.Panel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         button1 = new java.awt.Button();
-        button5 = new java.awt.Button();
+        jScrollPane_Accomplisments = new javax.swing.JScrollPane();
+        panel1 = new java.awt.Panel();
+        jScrollPane_PerGoals = new javax.swing.JScrollPane();
+        panel2 = new java.awt.Panel();
+        panel3 = new java.awt.Panel();
+        textArea1 = new java.awt.TextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Profile");
-        setMinimumSize(new java.awt.Dimension(600, 400));
-        setPreferredSize(new java.awt.Dimension(600, 400));
-        setSize(new java.awt.Dimension(600, 400));
+        setBackground(new java.awt.Color(229, 217, 182));
+        setMaximumSize(new java.awt.Dimension(1600, 900));
+        setMinimumSize(new java.awt.Dimension(800, 450));
+        setPreferredSize(new java.awt.Dimension(800, 450));
 
-        panel1.setPreferredSize(new java.awt.Dimension(250, 400));
+        jPanel1.setBackground(new java.awt.Color(40, 84, 48));
+        jPanel1.setPreferredSize(new java.awt.Dimension(800, 120));
 
-        jLabel_Profile.setFont(new java.awt.Font("Century Schoolbook", 1, 36)); // NOI18N
-        jLabel_Profile.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel_Profile.setText(" PROFILE ");
-        jLabel_Profile.setToolTipText("");
-        jLabel_Profile.setAlignmentY(0.0F);
-        jLabel_Profile.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        jLabel1.setFont(new java.awt.Font("Century Schoolbook", 0, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(229, 217, 182));
+        jLabel1.setText("Welcome, " + driver.getMain().getUser().getFirstName() );
 
-        button3.setLabel("button3");
+        button1.setBackground(new java.awt.Color(229, 217, 182));
+        button1.setFont(new java.awt.Font("Century Schoolbook", 0, 12)); // NOI18N
+        button1.setLabel("SET PROFILE");
+        button1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                button1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(179, 179, 179)
+                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(222, 222, 222))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(275, 275, 275))
+        );
+
+        panel1.setBackground(new java.awt.Color(164, 190, 123));
+        panel1.setPreferredSize(new java.awt.Dimension(250, 200));
 
         javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
         panel1.setLayout(panel1Layout);
         panel1Layout.setHorizontalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel_Profile, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
-                .addContainerGap(29, Short.MAX_VALUE)
-                .addComponent(button3, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27))
+            .addGap(0, 250, Short.MAX_VALUE)
         );
         panel1Layout.setVerticalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel_Profile, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 248, Short.MAX_VALUE)
-                .addComponent(button3, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18))
+            .addGap(0, 312, Short.MAX_VALUE)
         );
 
-        getContentPane().add(panel1, java.awt.BorderLayout.EAST);
+        jScrollPane_Accomplisments.setViewportView(panel1);
 
-        panel2.setBackground(new java.awt.Color(242, 242, 242));
-        panel2.setPreferredSize(new java.awt.Dimension(300, 400));
-
-        label3.setText("label3");
-
-        textField3.setText("textField3");
-
-        label_FNAME.setFont(new java.awt.Font("Century Schoolbook", 0, 16)); // NOI18N
-        label_FNAME.setPreferredSize(new java.awt.Dimension(58, 20));
-        label_FNAME.setText("FIRST NAME:");
-
-        textField_FName.setFont(new java.awt.Font("Century Schoolbook", 0, 14)); // NOI18N
-        textField_FName.setMinimumSize(new java.awt.Dimension(180, 22));
-
-        label_LName.setFont(new java.awt.Font("Century Schoolbook", 0, 16)); // NOI18N
-        label_LName.setPreferredSize(new java.awt.Dimension(58, 20));
-        label_LName.setText("LAST NAME:");
-
-        textField_LName.setFont(new java.awt.Font("Century Schoolbook", 0, 14)); // NOI18N
-        textField_LName.setMinimumSize(new java.awt.Dimension(180, 22));
-
-        label_DOB.setFont(new java.awt.Font("Century Schoolbook", 0, 16)); // NOI18N
-        label_DOB.setPreferredSize(new java.awt.Dimension(58, 20));
-        label_DOB.setText("DATE OF BIRTH:");
-
-        textField_DOB.setFont(new java.awt.Font("Century Schoolbook", 0, 14)); // NOI18N
-        textField_DOB.setMinimumSize(new java.awt.Dimension(180, 22));
-
-        label_PhoneNo.setFont(new java.awt.Font("Century Schoolbook", 0, 16)); // NOI18N
-        label_PhoneNo.setPreferredSize(new java.awt.Dimension(58, 20));
-        label_PhoneNo.setText("PHONE NO.");
-
-        textField_PhoneNo.setFont(new java.awt.Font("Century Schoolbook", 0, 14)); // NOI18N
-        textField_PhoneNo.setMinimumSize(new java.awt.Dimension(180, 22));
-
-        label_Major.setFont(new java.awt.Font("Century Schoolbook", 0, 16)); // NOI18N
-        label_Major.setPreferredSize(new java.awt.Dimension(58, 20));
-        label_Major.setText("MAJOR:");
-
-        textField_Major.setFont(new java.awt.Font("Century Schoolbook", 0, 14)); // NOI18N
-        textField_Major.setMinimumSize(new java.awt.Dimension(180, 22));
-
-        button4.setLabel("button4");
-
-        button2.setLabel("button2");
+        panel2.setBackground(new java.awt.Color(164, 190, 123));
+        panel2.setPreferredSize(new java.awt.Dimension(250, 200));
 
         javax.swing.GroupLayout panel2Layout = new javax.swing.GroupLayout(panel2);
         panel2.setLayout(panel2Layout);
         panel2Layout.setHorizontalGroup(
             panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(textField_FName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(textField_LName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(textField_DOB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(textField_PhoneNo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(textField_Major, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(label_Major, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(label_PhoneNo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(label_DOB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(label_LName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(label_FNAME, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(panel2Layout.createSequentialGroup()
-                        .addComponent(button4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(button2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap())
+            .addGap(0, 250, Short.MAX_VALUE)
         );
         panel2Layout.setVerticalGroup(
             panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panel2Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(label_FNAME, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField_FName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(label_LName, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField_LName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(label_DOB, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField_DOB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(label_PhoneNo, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField_PhoneNo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(label_Major, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField_Major, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(button4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(button2, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE))
-                .addContainerGap())
+            .addGap(0, 312, Short.MAX_VALUE)
         );
 
-        getContentPane().add(panel2, java.awt.BorderLayout.WEST);
+        jScrollPane_PerGoals.setViewportView(panel2);
 
-        panel3.setMinimumSize(new java.awt.Dimension(125, 100));
-        panel3.setPreferredSize(new java.awt.Dimension(150, 328));
+        panel3.setBackground(new java.awt.Color(95, 141, 78));
 
-        button1.setLabel("button1");
-
-        button5.setLabel("button1");
+        textArea1.setFont(new java.awt.Font("Edwardian Script ITC", 0, 20)); // NOI18N
 
         javax.swing.GroupLayout panel3Layout = new javax.swing.GroupLayout(panel3);
         panel3.setLayout(panel3Layout);
         panel3Layout.setHorizontalGroup(
             panel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 125, Short.MAX_VALUE)
+            .addGroup(panel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(textArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         panel3Layout.setVerticalGroup(
             panel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(panel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(textArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        getContentPane().add(panel3, java.awt.BorderLayout.CENTER);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane_Accomplisments, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane_PerGoals, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addGap(21, 21, 21))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane_Accomplisments, javax.swing.GroupLayout.DEFAULT_SIZE, 314, Short.MAX_VALUE)
+                    .addComponent(jScrollPane_PerGoals)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
 
         pack();
     }
 
-    private java.awt.Button button1;
-    private java.awt.Button button2;
-    private java.awt.Button button3;
-    private java.awt.Button button4;
-    private java.awt.Button button5;
-    private javax.swing.JLabel jLabel_Profile;
-    private java.awt.Label label3;
-    private java.awt.Label label_DOB;
-    private java.awt.Label label_FNAME;
-    private java.awt.Label label_LName;
-    private java.awt.Label label_Major;
-    private java.awt.Label label_PhoneNo;
-    private java.awt.Panel panel1;
-    private java.awt.Panel panel2;
-    private java.awt.Panel panel3;
-    private java.awt.TextField textField3;
-    private java.awt.TextField textField_DOB;
-    private java.awt.TextField textField_FName;
-    private java.awt.TextField textField_LName;
-    private java.awt.TextField textField_Major;
-    private java.awt.TextField textField_PhoneNo;
-}
+    private void button1ActionPerformed(java.awt.event.ActionEvent evt) {                                        
+        // TODO add your handling code here:
+        //driver.name = JOptionPane.showInputDialog(this, "Enter Name: ", JOptionPane.INFORMATION_MESSAGE);
+        //if (name.equals(JOptionPane.OK_OPTION)) {
+        driver.major = JOptionPane.showInputDialog(this, "What is your Major? ", JOptionPane.INFORMATION_MESSAGE);
+        //} 
+        driver.quote = JOptionPane.showInputDialog(this, "Enter your favourite quote!", JOptionPane.INFORMATION_MESSAGE);
+        driver.ProfileFileWriter();
 
+        
+        
+    }      
+}
