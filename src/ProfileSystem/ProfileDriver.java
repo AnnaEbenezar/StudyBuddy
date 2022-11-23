816591089fbc1b82afa5d41490ffdafdcce7d0da
PantilaK <88522737+PantilaK@users.noreply.github.com>
2022-11-23 07:54:00 +0700

    Update ProfileDriver.java
    Signed-off-by: PantilaK <88522737+PantilaK@users.noreply.github.com>

--- src/ProfileSystem/ProfileDriver.java
+++ src/ProfileSystem/ProfileDriver.java
@@ -5,25 +5,47 @@ import MainSystem.User;
 import Utility.ModuleDriver;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ProfileDriver implements ModuleDriver {
     private final MainDriver main;
     private static ProfileDriver instance = null;
     private volatile boolean runningFlag = false;
+
     private ProfileUI UI;
+
+    public Personal personal;
+
+    public Path path;
+
+    public User user;
+
+    public MainDriver getMain() {
+        return main;
+    }
 
     private ProfileDriver(MainDriver main) {
         this.main = main;
     }
 
     public static ProfileDriver getInstance(MainDriver main) {
-        if (instance == null) instance = new ProfileDriver(main);
+        if (instance == null)
+            instance = new ProfileDriver(main);
         return instance;
     }
 
     @Override
     public void quitModule() {
-        //quitting
+        // quitting
         runningFlag = false;
     }
 
@@ -34,11 +56,66 @@ public class ProfileDriver implements ModuleDriver {
 
     @Override
     public void run() {
-        UI = new ProfileUI(this);
-        this.runningFlag = true;
+        personal = new Personal();
 
+        getPath();
+
+        // starting
+        UI = new ProfileUI(this);
+        UI.setVisible(true);
+
+        readJSON();
+        this.runningFlag = true;
 
     }
 
-    
+    public void getPath() {
+        user = main.getUser();
+        String username = "resources/users/" + user.getUsername() + "/profile";
+        path = Paths.get(username);
+    }
+
+    public void installGoalInfo(String name, boolean isCheck) {
+        CheckBox cB = new CheckBox();
+        cB.setName(name);
+        cB.setCheck(isCheck);
+
+        personal.goals.add(cB);
+        writeJSON();
+    }
+
+    public void writeJSON() {
+        // path = Paths.get("resources/users/123456/schedule");
+
+        try (Writer writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(personal, writer);
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    public void readJSON() {
+        Gson gson = new Gson();
+        // path = Paths.get("resources/users/123456/schedule");
+
+        try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+
+            personal = gson.fromJson(reader, personal.getClass());
+
+            if (personal == null) {
+                personal = new Personal();
+            }
+
+            if (UI != null) {
+                UI.storeBackToJGoals();
+            }
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
 }
+
